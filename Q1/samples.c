1ef7f454a26d8a4e2ad74d4f62f546ca67c3f4be
Carolina Brandão <80838413+CarolBrandak@users.noreply.github.com>
2022-11-10 18:48:21 +0000

    Add files via upload

---
+++ Q1/samples.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int	main(int argc, char *argv[])
+{
+	if (argc != 4)
+	{
+		printf("usage: samples file numberfrags maxfragsize\n");
+		exit(EXIT_FAILURE);
+	}
+
+	return (0);
+}
