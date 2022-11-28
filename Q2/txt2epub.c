8ee7297d3f33c6642a41c15f1b991b30c5872ebc
tiago aleixo <tiago20aleixo@gmail.com>
2022-11-28 12:40:31 +0000

    changed system to execvp

--- Q2/txt2epub.c
+++ Q2/txt2epub.c
@@ -12,32 +12,58 @@ int main(int argc, char *argv[])
 	//FILE *file = fopen(argv[1], "r");
 	
 	int nArgv = argc;
-	pid_t pid = 1;
-	
-	//read n files 
-	for(int i=1; i<argc; i++) {
-		// create process and execute pandoc, exec 
-		// name, -o ficheiro.epub
+	pid_t pid = 1,wpid;
+	int status = 0;
+	if (pid = fork() == 0){
 		
-		//remove .txt terminator		
-		argv[i][sizeof(argv[i])-3] = '\0';
+		//read n files 
+		for(int i=1; i<argc; i++) {
+			// create process and execute pandoc, exec 
+			// name, -o ficheiro.epub
+			char *origin; //store origin file
+			asprintf(&origin,argv[i]);
+			
+			//remove .txt terminator		
+			argv[i][sizeof(argv[i])-3] = '\0';
+			
+			char * destination; //store destinantion file
+			asprintf(&destination,"%s.epub",argv[i]);
 
-		char *buffer;
-		asprintf(&buffer,"pandoc %s.txt --to=epub -o %s.epub --metadata title=\"%s\"", argv[i],argv[i],argv[i]);
+			char * title; //store title 
+			asprintf(&title,"title=\"%s\"",argv[i]);
 
-		//convert .txt to .epub
-		system(buffer); 
-	}
+			char *buffer;
+			asprintf(&buffer,"%s.txt --to=epub -o %s.epub --metadata title=\"%s\"", argv[i],argv[i],argv[i]);
 
-	//add n files to ebooks.zip
-	char tozip[100] = "zip ebooks.zip ";
 
-	for (int i = 1; i < nArgv; i++)
-	{
-		strcat(tozip, argv[i]);
-		strcat(tozip, ".epub ");
-	}
-	system(tozip);
+
+			char* argument_list[] = {"/bin/pandoc", origin, "--to=epub", "-o", destination, "--metadata", title, NULL};
+
+			//convert .txt to .epub
+			execvp("/bin/pandoc",argument_list); 
+		}
+	}else{
+
+	//wait for all subprocesses to finish
+	while ((wpid = wait(&status)) > 0);
+
 	
+	char tozip[100];//todo change this to adaptive size
+
+	//change terminator for output files
+	for (size_t i = 1; i < nArgv; i++)
+	{	
+		argv[i][sizeof(argv[i])-3] = '\0';
+		strcat(&tozip,argv[i]);
+		strcat(&tozip,".epub");
+	}
+	
+
+
+	char* argument_list[] = {"/bin/zip", "ebooks.zip", &tozip, NULL};
+	printf("%d\n",execvp("/bin/zip",argument_list)); 
+	printf("something\n");
+	
+	}
 	return(0);
 }
