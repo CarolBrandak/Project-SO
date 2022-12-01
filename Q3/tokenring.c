d1553ffa0c3484cd7275446eca81e1e217eace71
Carolina Brandão <up202004461@edu.fc.up.pt>
2022-12-01 23:54:28 +0000

    Porra para isto não esta acaba mas prontos

--- Q3/tokenring.c
+++ Q3/tokenring.c
@@ -2,6 +2,7 @@
 #include <stdlib.h>
 #include <sys/stat.h>
 #include <time.h>
+#include <string.h>
 
 
 struct stat buf;
@@ -9,7 +10,6 @@ struct stat buf;
 int main(int argc, char *argv[])
 {	
 	// Read 3 numbers
-
 	if (argc != 4)
 	{
 		printf("Error: Not enough info\n");
@@ -17,17 +17,21 @@ int main(int argc, char *argv[])
 	}
 
 	//srand(time(NULL));
-
 	int numberOfProcesses = atoi(argv[1]);
 	int probabilityBlocked = atoi(argv[2]);
 	int timeBlocked = atoi(argv[3]);
+
+	if (numberOfProcesses < 2)
+	{
+		printf("Error: Not enough processes\n");
+		exit(EXIT_FAILURE);
+	}
 
 	char i_char = 1 + '0'; 
 	char i1_char = 2 + '0';
 	char fifoName[9] = {'p', 'i', 'p', 'e', i_char, 't', 'o', i1_char};
 	
 	//Create the myfifo
-
 	for(int i=1; i<numberOfProcesses+1; i++) {
 		i_char = i + '0';
 		i1_char = (i+1) + '0';
@@ -62,7 +66,7 @@ int main(int argc, char *argv[])
 	
 	//Create ring of processes
 	for (int i = 1; i <= numberOfProcesses; i++) {	
-
+		//sleep(3);
 		char readFifo[9] = {'p', 'i', 'p', 'e', 'n', 't', 'o', 'n'};
 		char writeFifo[9] = {'p', 'i', 'p', 'e', 'n', 't', 'o', 'n'};
 
@@ -100,25 +104,38 @@ int main(int argc, char *argv[])
 				printf("started with token = %d \n",token);
 
 				int write_fifo = open(writeFifo);
-				write(write_fifo, token, sizeof(token));
+				write(write_fifo, &token, sizeof(token));
 				close(writeFifo);
 			}
-
+			double r;
 			while (1){
+
+				int token_r;
 				int read_fifo = open(readFifo);
-				read(read_fifo, token, sizeof(token));
+				if(read(read_fifo, token_r, sizeof(token_r)) == 0){
+					close(readFifo);
+					continue;
+				}
 				close(readFifo);
 
-				printf("read from = %s with token = %d \n",readFifo, token);
-				token++;
+				r = rand() % 101;
+				printf("random = %d\n", r);
 				
+				if ((r/100) < probabilityBlocked){
+					sleep(timeBlocked);
+					printf("lock on token (val = %d)\n", token_r);
+					printf("unlock token\n");
+				}
+
+				printf("read from = %s with token = %d \n",readFifo, token_r);
+				token_r++;
+
 				int write_fifo = open(writeFifo);
-				write(write_fifo, token, sizeof(token));
-				close(write_fifo);
+				write(write_fifo, &token_r, sizeof(token_r));
+				close(writeFifo);
+				printf("write to = %s with token = %d \n",writeFifo, token_r);
 
 			}
-
-			exit(0);
 		}
 	}
 
